72c275cd489dc0e2c091109498b580cda2c34cda
ZAM <ZAM@1CLICK.LV>
2023-12-06 23:28:12 +0200

    Using temp StoredProcedure for logic

--- GenerateRandomCodes.sql
+++ GenerateRandomCodes.sql
@@ -1,3 +1,23 @@
+----------
+
+CREATE PROCEDURE #GenerateRandomCode
+    @aValidChars NVARCHAR(50),
+	@aPassLength INT,
+    @aRandomCode NVARCHAR(6) OUTPUT
+AS
+BEGIN
+    SET @aRandomCode = '';
+
+    WHILE LEN(@aRandomCode) < @aPassLength
+    BEGIN
+        DECLARE @RandomIndex INT = ROUND(RAND() * (LEN(@aValidChars) - 1) + 1, 0);
+        SET @aRandomCode = @aRandomCode + SUBSTRING(@aValidChars, @RandomIndex, 1);
+    END
+END;
+GO
+
+----------
+
 DECLARE @CONST_TOTAL_PASSWORD_NEEDED as INT = 10;
 
 -- Create temp table
@@ -7,18 +27,18 @@ DECLARE @ValidChars as NVARCHAR(34) = 'ABCDEFGHIJKLMNPQRSTUVWXYZ123456789';
 
 DECLARE @i as INT = 1;
 
+-- Create a temporary stored procedure
+
+
 WHILE @i <= @CONST_TOTAL_PASSWORD_NEEDED
 BEGIN
 	DECLARE @RandomCode as NVARCHAR(6) = '';
 
-	WHILE LEN(@RandomCode) < 6
-	BEGIN
-		DECLARE @RandomIndex INT = ROUND(RAND() * (LEN(@ValidChars) - 1) + 1, 0);
-		SET @RandomCode = @RandomCode + SUBSTRING(@ValidChars, @RandomIndex, 1);
-	END
+	-- Execute the temporary stored procedure
+    EXEC #GenerateRandomCode @ValidChars, 6, @RandomCode OUTPUT;
 
-	INSERT INTO #RandomCodes (Code) VALUES (@RandomCode);
-
+    INSERT INTO #RandomCodes (Code) VALUES (@RandomCode);
+	
 	SET @i = @i + 1;
 END
 
@@ -27,3 +47,5 @@ SELECT * FROM #RandomCodes;
 
 -- Drop temp table
 DROP TABLE #RandomCodes;
+
+DROP PROCEDURE #GenerateRandomCode;
