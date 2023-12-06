682acbe710cfbb36147978964deb22a7c8d04b1c
ZAM <ZAM@1CLICK.LV>
2023-12-06 23:38:01 +0200

    Using Try Catch block

--- GenerateRandomCodes.sql
+++ GenerateRandomCodes.sql
@@ -1,51 +1,76 @@
 ----------
 
-CREATE PROCEDURE #GenerateRandomCode
-    @aValidChars NVARCHAR(50),
-	@aPassLength INT,
-    @aRandomCode NVARCHAR(6) OUTPUT
-AS
+IF OBJECT_ID('tempdb..#GenerateRandomCode', 'P') IS NOT NULL
 BEGIN
-    SET @aRandomCode = '';
-
-    WHILE LEN(@aRandomCode) < @aPassLength
-    BEGIN
-        DECLARE @RandomIndex INT = ROUND(RAND() * (LEN(@aValidChars) - 1) + 1, 0);
-        SET @aRandomCode = @aRandomCode + SUBSTRING(@aValidChars, @RandomIndex, 1);
-    END
+	-- Drop the temporary stored procedure
+	DROP PROCEDURE #GenerateRandomCode;
 END;
 GO
 
-----------
-
-DECLARE @CONST_TOTAL_PASSWORD_NEEDED as INT = 10;
-
--- Create temp table
-CREATE TABLE #RandomCodes (Code NVARCHAR(6));
-
-DECLARE @ValidChars as NVARCHAR(34) = 'ABCDEFGHIJKLMNPQRSTUVWXYZ123456789';
-
-DECLARE @i as INT = 1;
-
--- Create a temporary stored procedure
-
-
-WHILE @i <= @CONST_TOTAL_PASSWORD_NEEDED
+CREATE PROCEDURE #GenerateRandomCode
+	@aValidChars NVARCHAR(50),
+	@aPassLength INT,
+	@aRandomCode NVARCHAR(6) OUTPUT
+AS
 BEGIN
-	DECLARE @RandomCode as NVARCHAR(6) = '';
+	SET @aRandomCode = '';
 
-	-- Execute the temporary stored procedure
-    EXEC #GenerateRandomCode @ValidChars, 6, @RandomCode OUTPUT;
+	WHILE LEN(@aRandomCode) < @aPassLength
+	BEGIN
+		DECLARE @RandomIndex INT = ROUND(RAND() * (LEN(@aValidChars) - 1) + 1, 0);
+		SET @aRandomCode = @aRandomCode + SUBSTRING(@aValidChars, @RandomIndex, 1);
+	END
+END;
+GO
 
-    INSERT INTO #RandomCodes (Code) VALUES (@RandomCode);
+-- MAIN BODY
+
+BEGIN TRY
+
+	DECLARE @CONST_TOTAL_PASSWORD_NEEDED as INT = 10;
+
+	-- Create temp table
+	CREATE TABLE #RandomCodes (Code NVARCHAR(6));
+
+	DECLARE @ValidChars as NVARCHAR(34) = 'ABCDEFGHIJKLMNPQRSTUVWXYZ123456789';
+
+	DECLARE @i as INT = 1;
+
+	-- Create a temporary stored procedure
+
+
+	WHILE @i <= @CONST_TOTAL_PASSWORD_NEEDED
+	BEGIN
+		DECLARE @RandomCode as NVARCHAR(6) = '';
+
+		-- Execute the temporary stored procedure
+		EXEC #GenerateRandomCode @ValidChars, 6, @RandomCode OUTPUT;
+
+		INSERT INTO #RandomCodes (Code) VALUES (@RandomCode);
 	
-	SET @i = @i + 1;
-END
+		SET @i = @i + 1;
+	END
 
--- Display result
-SELECT * FROM #RandomCodes;
+	-- Display result
+	SELECT * FROM #RandomCodes;
 
--- Drop temp table
-DROP TABLE #RandomCodes;
+	-- Drop temp table
+	IF OBJECT_ID('tempdb..#RandomCodes', 'U') IS NOT NULL
+	BEGIN
+		-- Drop the temporary table
+		DROP TABLE #RandomCodes;
+	END
 
-DROP PROCEDURE #GenerateRandomCode;
+	-- Check if the temporary stored procedure exists
+	IF OBJECT_ID('tempdb..#GenerateRandomCode', 'P') IS NOT NULL
+	BEGIN
+		-- Drop the temporary stored procedure
+		DROP PROCEDURE #GenerateRandomCode;
+	END
+
+
+END TRY
+BEGIN CATCH
+    -- Handle the exception
+    PRINT 'An error occurred: ' + ERROR_MESSAGE();
+END CATCH
